a5f36bc9a8ce1b27c4f9908f3c3386132434cf96
Anıl Şenay <anilsenay3@gmail.com>
2020-09-15 13:38:49 +0300

    Update v0.1.4, see checklogs on Github for changes

--- src/index.tsx
+++ src/index.tsx
@@ -2,38 +2,50 @@ import * as React from "react";
 import Markdown from "markdown-to-jsx";
 
 interface Props {
-    file: string;
-    components: Array<React.FC | React.Component | any>;
+  file: string;
+  components?: Array<React.FC | React.Component | any>;
+  overrides?: object;
+  options?: object;
 }
 
-export const Rearkdown: React.FC<Props> = ({file, components}) => {
-    const [state, setState] = React.useState("");
+export const Rearkdown: React.FC<Props> = ({
+  file,
+  components = [],
+  overrides,
+  options,
+}) => {
+  const [state, setState] = React.useState("");
 
-    React.useEffect(() => {
-        fetch(file)
-            .then((res) => res.text())
-            .then((post) => setState(post))
-            .catch((err) => console.error(err));
-    }, [file]);
+  React.useEffect(() => {
+    fetch(file)
+      .then((res) => res.text())
+      .then((post) => setState(post))
+      .catch((err) => console.error(err));
+  }, [file]);
 
-    return (
-        <div>
-            {state && (
-                <Markdown
-                    children={state}
-                    options={{
-                        overrides:
-                            components &&
-                            Object.assign(
-                                {},
-                                ...components?.map((item: any) => {
-                                    const name: string = item.name;
-                                    return {[name]: {component: item}};
-                                })
-                            ),
-                    }}
-                />
-            )}
-        </div>
-    );
+  return (
+    <div>
+      {state && (
+        <Markdown
+          children={state}
+          options={{
+            slugify: (str) => str,
+            overrides:
+              components.length > 0
+                ? Object.assign(
+                    { ...overrides },
+                    ...components?.map((item: any) => {
+                      const name: string = item.name;
+                      return { [name]: { component: item } };
+                    })
+                  )
+                : {
+                    ...overrides,
+                  },
+            ...options,
+          }}
+        />
+      )}
+    </div>
+  );
 };
